46782a53d555e78bfeb9ace5e0af4fc691a97883
AthulKrishz <athulkrishnacv1234@gmail.com>
2024-01-05 00:21:00 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,14 @@
+let name="hello world";
+console.log(name);
+let selectedcolour=['red','blue','green'];
+selectedcolour[3]='black';
+console.log(selectedcolour);
+function greet(_name){
+
+    console.log('hello '+_name);
+}
+greet('parzival');
+function square(number){
+    return number*number;
+}
+console.log(square(2));
